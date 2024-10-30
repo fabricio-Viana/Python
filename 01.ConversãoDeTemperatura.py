707439b97776cf577bd44b87048d5ce04cdab05b
Fabricio Viana <145355057+fabricio-Viana@users.noreply.github.com>
2024-10-30 12:49:23 -0300

    Create 01.ConversãoDeTemperatura.py

---
+++ 01.ConversãoDeTemperatura.py
@@ -0,0 +1,6 @@
+F = input("Digite uma temperatura em Fahrenhei ") 
+C =  (int(F) - 32) * 5 / 9
+
+print("---------------------------------------")
+print("Convertendo Fahrenhei para Celsius..." )
+print("A temperatura de", F,"Fahrenhei em Celsius é de ",C )
